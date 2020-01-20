ad02d92668f98e7153e24d60ab6da68d1863bffd
Bmbus <jannes.t13@gmail.com>
2020-01-20 15:26:27 +0100

    Add base stuff for small screen

---
+++ small_screen/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+#@app.route("/calendar")
+#def calendar():
+#    return render_template("calendar.html")
+#
+#@app.route("/weather")
+#def weather():
+#    return render_template("weather.html")
+
+if __name__ == "__main__":
+    app.run(port=8080, debug=True)
