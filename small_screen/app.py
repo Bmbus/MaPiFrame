3f1f9fe9b2db15f81082228e4a20295123516fac
Bmbus <jannes.t13@gmail.com>
2020-01-23 19:45:25 +0100

    Add more weather and calendar route

--- small_screen/app.py
+++ small_screen/app.py
@@ -6,13 +6,13 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-#@app.route("/calendar")
-#def calendar():
-#    return render_template("calendar.html")
-#
-#@app.route("/weather")
-#def weather():
-#    return render_template("weather.html")
+@app.route("/calendar")
+def calendar():
+    return render_template("calendar.html")
+
+@app.route("/weather")
+def weather():
+    return render_template("weather.html")
 
 if __name__ == "__main__":
     app.run(port=8080, debug=True)
