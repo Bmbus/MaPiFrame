0c93bac17159236df0506bd9c795d73298c67bcc
Bmbus <jannes.t13@gmail.com>
2020-01-24 17:55:26 +0100

    Add weather.py file to small_screen
    + add animation.html and some other little changes
    
    NOTE: small screen isn't working yet!

--- small_screen/app.py
+++ small_screen/app.py
@@ -1,4 +1,10 @@
 from flask import Flask, render_template
+from static.pyfiles import weather
+import json
+
+with open("../config.json") as fp:
+    file = json.load(fp)
+    location = file["city"]
 
 app = Flask(__name__)
 
@@ -11,8 +17,13 @@ def calendar():
     return render_template("calendar.html")
 
 @app.route("/weather")
-def weather():
-    return render_template("weather.html")
+def weather_route():
+    return render_template("weather.html", location=location, temp=weather.get_temp())
+
+@app.route("/animation")
+def animation():
+    return render_template("animation.html")
+
 
 if __name__ == "__main__":
     app.run(port=8080, debug=True)
