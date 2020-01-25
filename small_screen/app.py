9f71179e51d996ea47406083a0801070a7ca3c29
Jannes <36892200+Bmbus@users.noreply.github.com>
2020-01-25 17:31:17 +0100

    Add calendar.py
    Update .gitignore and app.py

--- small_screen/app.py
+++ small_screen/app.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template
-from static.pyfiles import weather
+from static.pyfiles import weather, calendar
 import json
+
+CALANDER_EVENT_START = calendar.get_events()["start"][:5]
+CALANDER_EVENT = calendar.get_events()["event"]
+CALANDER_EVENT_END = calendar.get_events()["end"][:5]
+CALANDER_EVENT_DT = calendar.get_events()["event_dt"]
+PERIOD = f"{CALANDER_EVENT_START} - {CALANDER_EVENT_END}"
 
 with open("../config.json") as fp:
     file = json.load(fp)
@@ -13,8 +19,12 @@ def index():
     return render_template("index.html")
 
 @app.route("/calendar")
-def calendar():
-    return render_template("calendar.html")
+def calendar_route():
+    return render_template("calendar.html",
+                            event_date = CALANDER_EVENT_DT,
+                            period = PERIOD,
+                            event = calendar.get_events()["event"]
+                            )
 
 @app.route("/weather")
 def weather_route():
