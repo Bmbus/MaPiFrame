2765437dd92b3efd925026f85606502564100299
Jannes <36892200+Bmbus@users.noreply.github.com>
2020-02-04 20:14:44 +0100

    Add system uptime page

--- small_screen/app.py
+++ small_screen/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from static.pyfiles import weather, calendar, room_stuff
 import json
+import uptime
 
 CALANDER_EVENT_START = calendar.get_events()["start"][:5]
 CALANDER_EVENT = calendar.get_events()["event"]
@@ -34,10 +35,23 @@ def weather_route():
 def animation():
     return render_template("animation.html")
 
+@app.route("/news")
+def news_route():
+    # todo: return news from TAGESSCHUA ?? read out rss feed: http://www.tagesschau.de/xml/rss2
+    return render_template("news.html")
+
 @app.route("room")
 def room():
     return render_template("room.html", hum=room_stuff.get_roomstuff()["hum"] ,temp=room_stuff.get_roomstuff()["temp"])
 
+@app.route("/system_info")
+def system_info():
+    return render_template("system.html", system_uptime=uptime.uptime())
+
+@app.route("/config")
+def config_route():
+    return render_template("config.html")
+    # todo: show config - extra page
 
 if __name__ == "__main__":
     app.run(port=8080, debug=True)
